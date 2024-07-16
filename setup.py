045382442f3a8551495eba71231cd5199263709f
yakupaslantas <martyrerdixon@gmail.com>
2024-07-16 23:00:41 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+        name = "rootchecker",
+        version = "0.1",
+        description = "Simple rootchecker for Android that written in python",
+        author = "yakupaslantas",
+        packages = find_packages(),
+)
+
+
